ff7d23427c9c94c39b39a80a1b87e39a95f189fe
Diego2 <diego2@contabilidad.com>
2021-05-17 22:51:09 -0500

    add frontend prod compilation

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ app.get("/info", (req, res) => {
             <p>${new Date()}</p>`);
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, () => {
   console.log(`Server is listening in port ${PORT}`);
